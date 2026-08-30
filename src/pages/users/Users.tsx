b1656c8d7c784bfb95a235ec98d410486149564c
Nowshin Reza <nowshin.reza.09.10.2001@gmail.com>
2026-08-31 00:46:17 +0600

    fix css

--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -4,7 +4,7 @@ import "./Users.scss";
 import { useState } from "react";
 import Add from "../../components/add/Add";
 import { userRows } from "../../data";
-// import { useQuery } from "@tanstack/react-query";
+//import { useQuery } from "@tanstack/react-query";
 
 const columns: GridColDef[] = [
   { field: "id", headerName: "ID", width: 90 },
